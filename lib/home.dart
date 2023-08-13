28579bf92011dc9826cfcc6f658b8e63fbfd4d1d
webneshin <sajjad.eb@gmail.com>
2023-08-13 23:37:53 +0330

    access to static getx controller

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("GetX Task List"),
+        title: Text(HomeController.to.screenName),
       ),
       body: SafeArea(
         child: GetBuilder(
